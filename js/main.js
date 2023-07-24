3b402b852ba58f270967e4bfa760acd49370f1f6
PatriciaOT <ot.patricia1@gmail.com>
2023-07-24 10:36:38 -0600

    Cambios

--- js/main.js
+++ js/main.js
@@ -1,22 +1,92 @@
-//Calculadora
-let btn0= document.getElementById("btn0");
-let btn1= document.getElementById("btn1");
-let btn2= document.getElementById("btn2");
-let btn3= document.getElementById("btn3");
-let btn4= document.getElementById("btn4");
-let btn5= document.getElementById("btn5");
-let btn6= document.getElementById("btn6");
-let btn7= document.getElementById("btn7");
-let btn8= document.getElementById("btn8");
-let btn9= document.getElementById("btn9");
-let btnPumto= document.getElementById("btnPunto");
-let btnIgual= document.getElementById("btnIgual");
-let btnSuma= document.getElementById("btnSuma");
-let btnResta= document.getElementById("btnResta");
-let btnMultiplica= document.getElementById("btnMultiplica");
-let btnDivide= document.getElementById("btnDivide");
+//index
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual")
+const botonesNumeros = document.querySelectorAll(".numero");
+const botonesOperadores = document.querySelectorAll(".operador");
 
-let txtResultado = document.getElementById("txtResultado");
-let lblDatos = document.getElementById("lbDatos");
 
-//presionar los numeros y que se vaya concatenando en el cuadro de texto; cada numero tendra su evento; una vez que yo presiono una de las operaciones permitidas, me debe escribir en el labell el numero que tengo y la operación, posteriormente cuando esceribo un nuevo numero, no lo debe concatenar, sino q lo reemplaza donde el 12 esta almacenado y la operacion tambien, cuando yo le presione igual me dará el resultado y la almacenacion de los numeros pasados junto con sus operadores aritméticos
+const display= new Display (displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton=>{
+    boton.addEventListener("click", () => display.agregarNumero(boton.innerHTML));
+}); //Botones numeros
+
+botonesOperadores.forEach(boton=>
+    {
+        boton.addEventListener("click", () => display.computar (boton.value))
+    }); //botones operadores
+
+
+    class Display {
+        constructor(displayValorAnterior, displayValorActual) {
+            this.displayValorActual = displayValorActual;
+            this.displayValorAnterior = displayValorAnterior;
+            this.calculador = new Calculadora();
+            this.tipoOperacion = undefined;
+            this.valorActual = '';
+            this.valorAnterior = '';
+            this.signos = {
+                sumar: '+',
+                dividir: '%',
+                multiplicar: 'x',
+                restar: '-', 
+            }
+        }
+    
+        borrar() {
+            this.valorActual = this.valorActual.toString().slice(0,-1);
+            this.imprimirValores();
+        }
+    
+        borrarTodo() {
+            this.valorActual = '';
+            this.valorAnterior = '';
+            this.tipoOperacion = undefined;
+            this.imprimirValores();
+        }
+    
+        computar(tipo) {
+            this.tipoOperacion !== 'igual' && this.calcular();
+            this.tipoOperacion = tipo;
+            this.valorAnterior = this.valorActual || this.valorAnterior;
+            this.valorActual = '';
+            this.imprimirValores();
+        }
+    
+        agregarNumero(numero) {
+            if(numero === '.' && this.valorActual.includes('.')) return
+            this.valorActual = this.valorActual.toString() + numero.toString();
+            this.imprimirValores();
+        }
+    
+        imprimirValores() {
+            this.displayValorActual.textContent = this.valorActual;
+            this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+        }
+    
+        calcular() {
+            const valorAnterior = parseFloat(this.valorAnterior);
+            const valorActual = parseFloat(this.valorActual);
+    
+            if( isNaN(valorActual)  || isNaN(valorAnterior) ) return
+            this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+        }
+    } //display
+
+    class Calculadora {
+        sumar(num1, num2) {
+            return num1 + num2;
+        }
+    
+        restar(num1, num2) {
+            return num1 - num2;
+        }
+    
+        dividir(num1, num2) {
+            return num1 / num2;
+        }
+    
+        multiplicar(num1, num2) {
+            return num1 * num2;
+        }
+    } 
